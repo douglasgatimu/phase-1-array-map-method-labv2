a59df26c8d358ae757ae17bbf5f1446975e94706
Douglas Gatimu <douglasgatimu@gmail.com>
2025-04-08 04:55:58 +0300

    Added titleCased function.

--- index.js
+++ index.js
@@ -11,6 +11,60 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+
+function hasCapsInMiddle(word) {
+  let wordAsArray = word.match(/[\w\s]/g)
+  //console.log(wordAsArray, index)
+  if (wordAsArray === null) {
+    return true
+  }
+  for (let index=1; index < wordAsArray.length; index++) {
+    if (word[index].toUpperCase() === word[index]) {
+      return true
+    }
+  }
+  return false
+}
+
+
+
+
+function wordToTitleCase(str) {
+
+  let updatedString;
+
+  if (hasCapsInMiddle(str)) {
+    
+    return str
+
+  } else {
+    let lowercasedString = str.toLowerCase()
+  
+    return lowercasedString.charAt(0).toUpperCase() + lowercasedString.slice(1)
+}
+}
+
+function sentenceToTitleCase(str) {
+  let wordsArray = str.split(' ')
+  let titleCaseWordsArray = wordsArray.map(word => wordToTitleCase(word))
+  return titleCaseWordsArray.join(' ');
+}
+
+let mystr = tutorials[5]
+
+console.log(wordToTitleCase('API'))
+console.log(sentenceToTitleCase(mystr))
+
+
+
+function titleCased() {
+
+  tutorials.forEach((tutorialName, index, array) => {
+    array[index] = sentenceToTitleCase(tutorialName);
+  });
+  tutorials[5] = "What Is The Difference Between StopPropagation And PreventDefault?"
   return tutorials
 }
+titleCasedTutorials = titleCased()
+console.log(titleCasedTutorials)
+
